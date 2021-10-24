3f024998d4e7e7841a52bf9908119cbec0255b95
My name <me@example.com>
2021-10-24 14:29:21 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,18 @@
+import {handleSubmit} from "./js/formHandler"
+import {checkUrl} from "./js/urlChecker"
+
+
+
+// TODO: include your scss file here
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
+// alert("HELLO FROM WEBPACK");
+export{
+    checkUrl,
+    handleSubmit
+}
+
+
---
+++ src/client/js/urlChecker.js
@@ -0,0 +1,25 @@
+// TODO declare a function to check the url ..
+//  The question now how can i do it. I understand you are very busy mate so here's some hints pick the one you like
+/* HINTS
+    1. https://gist.github.com/franciskim/41a959f8e3989254ef5d
+    2. https://www.tutorialspoint.com/How-to-validate-URL-address-in-JavaScript
+    3. https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
+    4. https://www.codegrepper.com/code-examples/javascript/javascript+validate+url
+    5. https://stackoverflow.com/a/5717133/6483379
+    6. https://www.npmjs.com/package/valid-url
+ */
+
+    function checkUrl(string) {
+        let  res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+        // console.log("the URL return ",res)
+        if(res !== null){
+            // return console.log("please enter Valid URL")
+            // alert("please enter Valid URL")
+            return true
+        }else{
+            return false
+        }
+        // return (res === null)
+      };
+    
+    export { checkUrl }
